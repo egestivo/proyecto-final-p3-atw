f39f04f214a6c225902aa4b79c6c4dfe3e929db6
jhoanssp <121051173+jhoanssp@users.noreply.github.com>
2025-08-02 20:44:50 -0500

    Update RepositoryInterface.php

--- src/interfaces/RepositoryInterface.php
+++ src/interfaces/RepositoryInterface.php
@@ -5,12 +5,7 @@ declare(strict_types=1);
 namespace App\interfaces;
 
 interface RepositoryInterface{
-    public function create(object $entity): bool;
-    public function findByid(int $id): ?object;//no se sabe que va a devolver
 
-    public function update(object $entity): bool;
-    public function delete(int $id): bool;
 
-    public function findAll(): array;//devuelve un arreglo de entidades
 
 }
